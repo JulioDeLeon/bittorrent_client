f4611fecd66d4ece26655cc1f22ff45cddf91b58
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2017-04-06 19:47:13 -0500

    investigating server spawn issues

--- lib/bittorrent_client/server.ex
+++ lib/bittorrent_client/server.ex
@@ -7,9 +7,30 @@ defmodule BittorrentClient.Server do
     GenServer.start_link(
       __MODULE__,
       db_dir,
-      name: {:global, {BittorrentClient.Server, name}}
+      name: {:global, {:btc_server, name}}
     )
   end
+  def list_current_torrents(serverName) do
+    IO.puts "Entered list_current_torrents"
+    GenServer.call(serverName, {:list_current_torrents, []})
+  end
+
+  def add_new_torrent(torrentFile) do
+    IO.puts "Entered add_new_torrent #{torrentFile}"
+  end
+
+  def delete_torrent_by_id(id) do
+    IO.puts "Entered delete_torrent_by id #{id}"
+  end
+
   # handle_call
+  def handle_call({:list_current_torrents}, _, _) do
+    {
+      :reply,
+      :ok,
+      "Successful call to list torrent"
+    }
+  end
+ 
   # handle_cast
 end
