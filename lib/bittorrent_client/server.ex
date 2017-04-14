258a08085b580df629ba60d9d0f48a0e6c9d8173
Julio de Leon <jangelod@gmail.com>
2017-04-13 20:35:18 -0500

    fixed some logic in torrent management

--- lib/bittorrent_client/server.ex
+++ lib/bittorrent_client/server.ex
@@ -33,22 +33,50 @@ defmodule BittorrentClient.Server do
     GenServer.call(:global.whereis_name({:btc_server, serverName}), {:delete_by_id, id})
   end
 
-  # handle_call
+  def update_torrent_status_by_id(serverName, id, status) do
+    IO.puts "Entered update_torrent_status_by_id"
+    GenServer.call(:global.whereis_name({:btc_server, serverName}), {:update_by_id, id, status})
+  end
+
+  def delete_all_torrents(serverName) do
+    IO.puts "Entered delete_all_torrents"
+    GenServer.call(:global.whereis_name({:btc_server, serverName}), {:delete_all})
+  end
   def handle_call({:list_current_torrents}, _from, {db, serverName, torrents}) do
     {:reply, torrents, {db, serverName, torrents}}
    # {status, actions, new state}
   end
 
   def handle_call({:add_new_torrent, torrentFile}, _from, {db, serverName, torrents}) do
-    random_id =:rand.uniform(1000)
-    torrents = Map.put(torrents, random_id, {"torrentFile", "init"})
-    {:reply, torrents, {db, serverName, torrents}}
+    id = :crypto.hash(:md5, torrentFile) |> Base.encode32
+    if not Map.has_key?(torrents, id) do
+      torrents = Map.put(torrents, id, {torrentFile, "init"})
+      {:reply, torrents, {db, serverName, torrents}}
+    else
+      {:reply, "That torrent already exist", {db, serverName, torrents}}
+    end
   end
 
   def handle_call({:delete_by_id, id}, _from, {db, serverName, torrents}) do
-    torrents = Map.delete(torrents, id)
-    {:reply, torrents, {db, serverName, torrents}}
+    if Map.has_key?(torrents, id) do
+      torrents = Map.delete(torrents, id)
+      {:reply, torrents, {db, serverName, torrents}}
+    else
+      {:reply, "Bad ID was given", {db, serverName, torrents}}
+    end
   end
 
-  # handle_cast (asynchronous)
+  def handle_call({:update_by_id, id, status}, _from, {db, serverName, torrents}) do
+    if Map.has_key?(torrents, id) do
+      torrents = Map.update!(torrents, id, fn {file, _} -> {file, status} end)
+      {:reply, torrents, {db, serverName, torrents}}
+    else
+      {:reply, "Bad ID was given", {db, serverName, torrents}}
+    end
+  end
+
+  def handle_call({:delete_all}, _from, {db, serverName, torrents}) do
+    torrents = Map.drop(torrents, Map.keys(torrents))
+    {:reply, torrents, {db, serverName, torrents}}
+  end
 end
