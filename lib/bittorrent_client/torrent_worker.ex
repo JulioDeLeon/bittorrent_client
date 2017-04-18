30c84daf9aec6fad300e6dd6d8d02f764567206d
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2017-04-18 08:52:04 -0500

    started making progress on the torrent portion of project

--- lib/bittorrent_client/torrent_worker.ex
+++ lib/bittorrent_client/torrent_worker.ex
@@ -1,18 +1,32 @@
 defmodule BittorrentClient.TorrentWorker do
   use GenServer
+  @moduledoc """
+  TorrentWorker handles on particular torrent magnet, manages the connections allowed and other settings. 
+  """
 
   def start_link(filename) do
     IO.puts "Starting torrent worker for #{filename}"
+	torrentMetadata = filename
+    |> File.read!()
+    |> Bento.torrent!()
+
     GenServer.start_link(
       __MODULE__,
-      filename,
-      name: {:global, {BittorrentClient.TorrentWorker, filename}}
-    )    
+      torrentMetadata,
+      name: {:global, {:btc_torrentworker, filename}}
+    )
   end
-  # handle_call
-  # handle_cast
-  def init(filename) do
-    IO.puts "Opening #{filename}"
-    {:ok, filename}
+
+  def init(torrentMetadata) do
+    {:ok, torrentMetadata}
+  end
+
+  def whereis(name) do
+    :global.whereis_name({:btc_torrentworker, name})
+  end
+
+  defp createTrackerRequest(url, params) do
+   	urlParams = for key <- Map.keys(params), do: "#{key}" <> "=" <> "#{Map.get(params, key)}"
+    URI.encode(url <> "?" <> Enum.join(urlParams, "&"))
   end
 end
