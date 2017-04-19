95fb519c7046a6cdfc8791e04f8c5a579953b870
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2017-04-19 14:07:51 -0500

    working on hooking the torrent supervisor to server

--- lib/bittorrent_client/torrent_supervisor.ex
+++ lib/bittorrent_client/torrent_supervisor.ex
@@ -1,13 +1,17 @@
 defmodule BittorrentClient.TorrentSupervisor do
   use Supervisor
   # start_link
-  def start_link(filename) do
+  def start_link(server, filename) do
     IO.puts "Starting torrent Supervisor for #{filename}"
     Supervisor.start_link(__MODULE__,filename)
   end
 
   # init -> supervise
-  def init(filename) do
-    
+  def init(server, filename) do
+    children = [
+      worker(BittorrentClient.TorrentWorker, [server, filename], id: {:btc_torrentworker, filename})
+    ]
+
+    supervise(children, strategy: :one_for_one)
   end
 end
