f2def7f7dceaa5996c7adcdd88b5452168b1aa8f
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2017-04-19 18:14:22 -0500

    investigating dynamic supervision

--- lib/bittorrent_client/torrent_supervisor.ex
+++ lib/bittorrent_client/torrent_supervisor.ex
@@ -1,17 +1,13 @@
 defmodule BittorrentClient.TorrentSupervisor do
   use Supervisor
   # start_link
-  def start_link(server, filename) do
-    IO.puts "Starting torrent Supervisor for #{filename}"
-    Supervisor.start_link(__MODULE__,filename)
+  def start_link() do
+    IO.puts "Starting torrent Supervisor"
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   # init -> supervise
-  def init(server, filename) do
-    children = [
-      worker(BittorrentClient.TorrentWorker, [server, filename], id: {:btc_torrentworker, filename})
-    ]
+  def init(:ok) do
 
-    supervise(children, strategy: :one_for_one)
   end
 end
