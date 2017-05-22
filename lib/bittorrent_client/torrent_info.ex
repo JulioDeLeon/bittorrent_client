ee3c28b7e604642a9e7ce1ff0abdba24c35abeeb
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2017-05-22 13:52:11 -0500

    removed unneed file

--- lib/bittorrent_client/torrent_info.ex
+++
@@ -1,8 +0,0 @@
-defmodule BittorrentClient.TorrentInfo do
-  @moduledoc """
-  Torrent Info defines data structure for torrent information
-  """
-
-  @derive [Poison.Encoder]
-  defstruct [:id, :file, :pid, :status]
-end
