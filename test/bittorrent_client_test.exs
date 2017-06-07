ed0a9eb001155be87c70fa75606e5d8afef2cdb3
Julio de Leon <jangelod@gmail.com>
2017-06-07 06:40:20 -0500

    added hard example of sha1 needed for debian torrent

--- test/bittorrent_client_test.exs
+++ test/bittorrent_client_test.exs
@@ -19,3 +19,5 @@ defmodule BittorrentClientTest do
 
   # create test for post and put request
 end
+
+# 934994	374.234654	192.168.0.15	130.239.18.159	HTTP	333	GET /announce?compact=0&downloaded=0&event=started&info_hash=%A2L%157%84%DBE4F%B3%C3%16%04m%5C%F6x%E1%DC%A5&left=660602880&no_peer_id=0&numwant=0&peer_id=-ET0001-&port=6969&uploaded=0 HTTP/1.1
