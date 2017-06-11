7ead505208e341ab73427c9e38f2c3f4c74575e6
Julio de Leon <jangelod@gmail.com>
2017-06-10 22:23:41 -0500

    changed test logic for infohash

--- test/bittorrent_client_test.exs
+++ test/bittorrent_client_test.exs
@@ -19,7 +19,6 @@ defmodule BittorrentClientTest do
 
   # create test for post and put request
   test "compute info hash" do
-    require Logger
     file = "examples/debian2.torrent"
     metadata = file
     |> File.read!()
@@ -39,9 +38,6 @@ defmodule BittorrentClientTest do
     end
   end
 end
-
-# 934994	374.234654	192.168.0.15	130.239.18.159	HTTP	333	GET /announce?compact=0&downloaded=0&event=started&info_hash=%A2L%157%84%DBE4F%B3%C3%16%04m%5C%F6x%E1%DC%A5&left=660602880&no_peer_id=0&numwant=0&peer_id=-ET0001-&port=6969&uploaded=0 HTTP/1.1
-
 
 # 139	32.479680465	192.168.0.3	130.239.18.159	HTTP	424
 # GET /announce?
