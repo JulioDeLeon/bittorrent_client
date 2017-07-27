4f615022ef3bcb2db8d92322372be4c7429719ae
Julio de Leon <jangelod@gmail.com>
2017-07-26 22:02:37 -0500

    fixing inifinite nif

--- c_src/torrent_worker_nif.c
+++ c_src/torrent_worker_nif.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*
   https://github.com/lita/bittorrent/blob/master/peers.py
@@ -18,26 +19,36 @@
   ip = '.'.join(ip)
   peer = Peer(ip, port, mySocket, self.infoHash, self.peer_id)
   self.peers.append(peer)
- */
+
+  example: <<78, 132, 52, 74, 192, 8>>
+*/
 
 static ERL_NIF_TERM
 get_peer_connection_info(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
   //load the passed binary
   int binary_size;
   ErlNifBinary temp_bin;
+  char* ipBuff = (char*) malloc(sizeof(char) * 8);
+  memset(ipBuff, '.', sizeof(char) * 8);
+  int portBuff = 0;
   if( !enif_get_int(env, argv[1], &binary_size)
       || !enif_inspect_binary(env, argv[0], &temp_bin)) {
     return enif_make_badarg(env);
   }
 
-  //TODO: create and ip and port for the passed binary
-  for(int x = 0; x < binary_size; x++) {
-    printf("%d:%c\n", x, temp_bin.data[x]);
+  //Get IP
+  for(int x = 0; x < binary_size + 4; x+2) {
+    //printf("%d:%c\n", x, temp_bin.data[x]);
+    ipBuff[x] = temp_bin.data[x];
   }
 
+  //Get Port
+  portBuff = atoi((const char *) &temp_bin.data[4]) * 256 + atoi((const char*) &temp_bin.data[5]);
+
   //return tuple {ip, port}
-  ERL_NIF_TERM retIP = enif_make_string(env, "temp", ERL_NIF_LATIN1);
-  ERL_NIF_TERM retPort = enif_make_uint(env, 0);
+  ERL_NIF_TERM retIP = enif_make_string(env, ipBuff, ERL_NIF_LATIN1);
+  ERL_NIF_TERM retPort = enif_make_uint(env, portBuff);
+  free(ipBuff);
   return enif_make_tuple(env, 2, retIP, retPort);
 }
 
