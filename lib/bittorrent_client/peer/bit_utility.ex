f5df35d0223c357b7989532357b3f304a9102b15
Julio de Leon <jangelod@gmail.com>
2018-01-12 09:24:09 -0600

    Still working on bit utilities

--- lib/bittorrent_client/peer/bit_utility.ex
+++ lib/bittorrent_client/peer/bit_utility.ex
@@ -7,6 +7,9 @@ defmodule BittorrentClient.Peer.BitUtility  do
     bit_index = @byte_size - (rem(pos, @byte_size) - 1)
     <<desired_byte_val>> = String.at(bitstr, bitstring_index)
     new_byte = <<Bitwise.|||(desired_byte_val, Bitwise.<<<(1, bit_index))>>
+    {before, temp} = String.split_at(bitstr, bitstring_index)
+    {_oldVal, aft} = String.split_at(temp, 1)
+    {:ok, before <> <<new_byte>> <> aft}
   end
 
   def set_bit(bitstr, 0, pos) when byte_size(bitstr) > 0 do
@@ -14,9 +17,16 @@ defmodule BittorrentClient.Peer.BitUtility  do
     bit_index = @byte_size - (rem(pos, @byte_size) - 1)
     <<desired_byte_val>> = String.at(bitstr, bitstring_index)
     new_byte = <<Bitwise.&&&(desired_byte_val, Bitwise.<<<(0, bit_index))>>
+    {before, temp} = String.split_at(bitstr, bitstring_index)
+    {_oldVal, aft} = String.split_at(temp, 1)
+    {:ok, before <> <<new_byte>> <> aft}
   end
 
-  def set_bit(<<>>, _, _) do
-    <<>>
+  def set_bit(<<>>, _val, _pos) do
+    {:error, "invalid bitstring"}
+  end
+
+  def set_bit(_bitstr, val, _pos) do
+    {:error, "invalid valuew"}
   end
 end
