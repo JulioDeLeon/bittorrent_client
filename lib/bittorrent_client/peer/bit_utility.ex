2c562d007568d24de16b932b0b127d275fe6ead9
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2018-01-12 18:56:15 -0600

    working on bit util and test

--- lib/bittorrent_client/peer/bit_utility.ex
+++ lib/bittorrent_client/peer/bit_utility.ex
@@ -1,25 +1,43 @@
-defmodule BittorrentClient.Peer.BitUtility  do
+defmodule BittorrentClient.Peer.BitUtility do
   require Bitwise
+  require Logger
   @byte_size 8
 
-  def set_bit(bitstr, 1, pos) when byte_size(bitstr) > 0 do
-    bitstring_index = div(pos, @byte_size)
-    bit_index = @byte_size - (rem(pos, @byte_size) - 1)
-    <<desired_byte_val>> = String.at(bitstr, bitstring_index)
-    new_byte = <<Bitwise.|||(desired_byte_val, Bitwise.<<<(1, bit_index))>>
-    {before, temp} = String.split_at(bitstr, bitstring_index)
-    {_oldVal, aft} = String.split_at(temp, 1)
-    {:ok, before <> <<new_byte>> <> aft}
+  def set_bit(bitstr, 1, pos) do
+    size = byte_size(bitstr)
+
+    if pos < 0 || div(pos, @byte_size) - 1 > size do
+      {:error, "invalid position was given"}
+    else
+      bitstring_index = div(pos, @byte_size)
+      bit_index = @byte_size - rem(pos, @byte_size)
+      <<desired_byte_val>> = String.at(bitstr, bitstring_index)
+      new_byte = Bitwise.|||(desired_byte_val, Bitwise.<<<(1, bit_index - 1))
+      {before, temp} = String.split_at(bitstr, bitstring_index)
+      {_oldVal, aft} = String.split_at(temp, 1)
+      {:ok, before <> <<new_byte>> <> aft}
+    end
   end
 
-  def set_bit(bitstr, 0, pos) when byte_size(bitstr) > 0 do
-    bitstring_index = div(pos, @byte_size)
-    bit_index = @byte_size - (rem(pos, @byte_size) - 1)
-    <<desired_byte_val>> = String.at(bitstr, bitstring_index)
-    new_byte = <<Bitwise.&&&(desired_byte_val, Bitwise.<<<(0, bit_index))>>
-    {before, temp} = String.split_at(bitstr, bitstring_index)
-    {_oldVal, aft} = String.split_at(temp, 1)
-    {:ok, before <> <<new_byte>> <> aft}
+  def set_bit(bitstr, 0, pos) do
+    size = byte_size(bitstr)
+
+    if pos < 0 || div(pos, @byte_size) - 1 > size do
+      {:error, "invalid position was given"}
+    else
+      bitstring_index = div(pos, @byte_size)
+      bit_index = @byte_size - rem(pos, @byte_size)
+      <<desired_byte_val>> = String.at(bitstr, bitstring_index)
+
+      new_byte =
+        Bitwise.&&&(desired_byte_val, 255 - Bitwise.<<<(1, bit_index - 1))
+
+      {before, temp} = String.split_at(bitstr, bitstring_index)
+      {_oldVal, aft} = String.split_at(temp, 1)
+      what = <<new_byte>>
+      ret = before <> <<new_byte>> <> aft
+      {:ok, before <> <<new_byte>> <> aft}
+    end
   end
 
   def set_bit(<<>>, _val, _pos) do
@@ -27,6 +45,22 @@ defmodule BittorrentClient.Peer.BitUtility  do
   end
 
   def set_bit(_bitstr, val, _pos) do
-    {:error, "invalid valuew"}
+    {:error, "invalid value"}
+  end
+
+  def create_full_bitfield(num_pieces, piece_length) do
+    max_bits = num_pieces * piece_length
+    bytes_needed = div(max_bits, @byte_size) + 1
+    excess_bits = (bytes_needed * @byte_size) - max_bits
+    max_binary_value = Bitwise.<<<(1, max_bits + 1) - 1
+    initial_buffer = <<max_binary_value::size(bytes_needed)>>
+    #zero out invalid piece indexes
+    
+  end
+
+  def create_empty_bitfield(num_pieces, piece_length) do
+    max_bits = num_pieces * piece_length
+    bytes_needed = div(max_bits, @byte_size) + 1
+    <<0 :: size(bytes_needed)>>
   end
 end
--- test/peer/peer_test.exs
+++ test/peer/peer_test.exs
@@ -7,9 +7,47 @@ defmodule PeerTest do
     {:ok, sample_arrary: [2, 8, 17]}
   end
 
-  test "simple bitwise oprationg" do
-    l = <<0,2>>
-    rteff
-    e = BitUtility.set_bit()
+  test "simple bitwise operating setting <<0,2>>" do
+    l = <<0, 2>>
+    {status, c} = BitUtility.set_bit(l, 1, 8)
+    e = <<0, 130>>
+    assert(status == :ok)
+    assert(c == e)
+  end
+
+  test "simple bitwise operating unsetting <<0,130>>" do
+    l = <<0, 130>>
+    {status, c} = BitUtility.set_bit(l, 0, 8)
+    e = <<0, 2>>
+    assert(status == :ok)
+    assert(c == e)
+  end
+
+  test "simple bitwise operating unsetting <<130>>" do
+    l = <<130>>
+    {status, c} = BitUtility.set_bit(l, 0, 0)
+    e = <<2>>
+    assert(status == :ok)
+    assert(c == e)
+  end
+
+  test "simple bitwise operating unsetting <<0,25,3>>" do
+    l = <<0, 25, 3>>
+    {status, c} = BitUtility.set_bit(l, 0, 23)
+    e = <<0, 25, 2>>
+    assert(status == :ok)
+    assert(c == e)
+  end
+
+  test "empty buffer" do
+    l = <<>>
+    {status, _reason} = BitUtility.set_bit(l, 1, 44)
+    assert(status = :error)
+  end
+
+  test "invalid position" do
+    l = <<1, 1, 1, 1, 1>>
+    {status, _reason} = BitUtility.set_bit(l, 3, 9)
+    assert(status = :error)
   end
 end
