bb06819c5428b71be6da0ffee9e208c5be676f6c
Julio de Leon <jangelod@gmail.com>
2018-01-20 23:43:47 -0600

    fix delete all

--- test/server/server_test.exs
+++ test/server/server_test.exs
@@ -1,6 +1,7 @@
-defmodule BittorrentClient.ServerTest do
+defmodule ServerTest do
   use ExUnit.Case
   @server_impl Application.get_env(:bittorrent_client, :server_impl)
+  @torrent_impl Application.get_env(:bittorrent_client, :torrent_impl)
   @server_name Application.get_env(:bittorrent_client, :server_name)
   @file_name_1 "priv/ubuntu.torrent"
   @file_name_2 "priv/arch.torrent"
@@ -16,23 +17,129 @@ defmodule BittorrentClient.ServerTest do
       |> File.read!()
       |> Bento.decode!()
 
-    on_exit fn ->
-      IO.puts "#{__MODULE__} on_exit"
-      :ok
-    end
+    {:ok,
+     [
+       file_1_bento_contents: file_1_bento_contents,
+       file_2_bento_contents: file_2_bento_contents
+     ]}
+  end
 
-    {:ok, [file_1_bento_contents: file_1_bento_contents,
-           file_2_bento_contents: file_2_bento_contents]}
+  setup do
+    on_exit(fn ->
+      # attempt to delete torrents on server layer
+      @server_impl.delete_all_torrents(@server_name)
+      {_s, table} = @server_impl.list_current_torrents(@server_name)
+      IO.puts("Current table: #{inspect(table)}")
+    end)
   end
 
   test "Addition of a new torrent from Server layer", context do
-    {status, data} = @server_impl.add_new_torrent(@server_name, @file_name_1)
-    assert status == :ok
+    {add_torrent_status, resp_map} =
+      @server_impl.add_new_torrent(@server_name, @file_name_1)
+
+    assert add_torrent_status == :ok
+    torrent_id = Map.get(resp_map, "torrent id")
+
+    {list_torrent_status, torrent_table} =
+      @server_impl.list_current_torrents(@server_name)
+
+    #    IO.inspect torrent_table
+    assert list_torrent_status == :ok
+    assert Map.has_key?(torrent_table, torrent_id)
+
+    torrent_entry = Map.get(torrent_table, torrent_id)
+    assert Map.has_key?(torrent_entry, "data")
+    assert Map.has_key?(torrent_entry, "metadata")
+
+    metadata = Map.get(torrent_entry, "metadata")
+
+    assert compare_bento_data_to_metadata(
+             context.file_1_bento_contents,
+             metadata
+           )
   end
 
-  test "Deletion of all torrents from Server Layer", context do
-    {status, torrent_table} = @server_impl.list_current_torrents(@server_name)
-    IO.inspect torrent_table
-    assert status == :ok
+  test "Deletion of single torrent from Server Layer", context do
+    {add_torrent_status, resp_map} =
+      @server_impl.add_new_torrent(@server_name, @file_name_2)
+
+    assert add_torrent_status == :ok
+    torrent_id = Map.get(resp_map, "torrent id")
+
+    {torrent_info_status, torrent_info} =
+      @server_impl.get_torrent_info_by_id(@server_name, torrent_id)
+
+    assert torrent_info_status == :ok
+    assert Map.has_key?(torrent_info, "data")
+    assert Map.has_key?(torrent_info, "metadata")
+
+    metadata = Map.get(torrent_info, "metadata")
+
+    assert compare_bento_data_to_metadata(
+             context.file_2_bento_contents,
+             metadata
+           )
+
+    {deletion_status, ret_data} =
+      @server_impl.delete_torrent_by_id(@server_name, torrent_id)
+
+    assert deletion_status == :ok
+
+    pid = @torrent_impl.whereis(torrent_id)
+    assert pid == :undefined
+
+    {list_torrent_status, torrent_table} =
+      @server_impl.list_current_torrents(@server_name)
+
+    assert list_torrent_status == :ok
+    assert Map.has_key?(torrent_table, torrent_id) == false
+  end
+
+  test "Deletion of all torrents from the Server Layer" do
+    {add_torrent_status, resp_map} =
+      @server_impl.add_new_torrent(@server_name, @file_name_1)
+
+    assert add_torrent_status == :ok
+    torrent_id = Map.get(resp_map, "torrent id")
+
+    {add_torrent_status_2, resp_map_2} =
+      @server_impl.add_new_torrent(@server_name, @file_name_2)
+
+    assert add_torrent_status_2 == :ok
+    torrent_id_2 = Map.get(resp_map_2, "torrent id")
+
+    {list_current_status, torrent_table} =
+      @server_impl.list_current_torrents(@server_name)
+
+    assert list_current_status == :ok
+    assert Map.has_key?(torrent_table, torrent_id)
+    assert Map.has_key?(torrent_table, torrent_id_2)
+
+    {delete_all_status, data} = @server_impl.delete_all_torrents(@server_name)
+    assert delete_all_status == :ok
+    IO.puts("Ret from delete all: #{inspect(data)}")
+    assert @torrent_impl.whereis(torrent_id) == :undefined
+    assert @torrent_impl.whereis(torrent_id_2) == :undefined
+
+    {list_current_status, torrent_table} =
+      @server_impl.list_current_torrents(@server_name)
+
+    assert list_current_status == :ok
+    assert torrent_table == %{}
+  end
+
+  defp compare_bento_data_to_metadata(bento_data, metadata) do
+    Enum.reduce(Map.keys(bento_data), true, fn key, acc ->
+      bento_field_val = Map.get(bento_data, key)
+
+      case Map.has_key?(metadata, key) do
+        false ->
+          acc
+
+        true ->
+          metadata_field_val = Map.get(metadata, key)
+          acc && bento_field_val == metadata_field_val
+      end
+    end)
   end
 end
