950d3be1a12665ef58658edc0b6912b379e518a7
Julio de Leon <jangelod@gmail.com>
2018-01-22 23:27:44 -0600

    working on peer tests

--- test/peer/peer_test.exs
+++ test/peer/peer_test.exs
@@ -1,4 +1,22 @@
 defmodule PeerTest do
   use ExUnit.Case
   doctest BittorrentClient.Peer.GenServerImpl
+  @peer_impl Application.get_env(:bittorrent_client, :peer_impl)
+  @file_name_1 "priv/ubuntu.torrent"
+  alias BittorrentClient.Torrent.Supervisor, as: TorrentSupervisor
+  setup_all do
+    torrent_id = "some_id"
+    torrent_pid = TorrentSupervisor.start_child({torrent_id, @file_name_1})
+
+    on_exit(fn ->
+      TorrentSupervisor.terminate_child(torrent_id)
+    end)
+    {:ok, [torrent_id: torrent_id,
+           torrent_pid: torrent_pid
+          ]}
+  end
+
+  test "whereis should fail if given an invalid peer id from Peer layer" do
+    assert @peer_impl.whereis("invalid_id") == :undefined
+  end
 end
--- test/torrent/torrent_test.exs
+++ test/torrent/torrent_test.exs
@@ -2,5 +2,27 @@ defmodule TorrentTest do
   use ExUnit.Case
   doctest BittorrentClient.Server.GenServerImpl
   @torrent_impl Application.get_env(:bittorrent_client, :torrent_impl)
+  @server_impl Application.get_env(:bittorrent_client, :server_impl)
+  @server_name Application.get_env(:bittorrent_client, :server_name)
+  @file_name_1 "priv/ubuntu.torrent"
+  @file_name_2 "priv/arch.torrent"
   alias BittorrentClient.Torrent.Supervisor, as: TorrentSupervisor
+
+  setup do
+    on_exit(fn ->
+      _ret = @server_impl.delete_all_torrents(@server_name)
+    end)
+  end
+
+  test "whereis should return a valid pid from Torrent layer", context do
+    {add_status, data} = @server_impl.add_new_torrent(@server_name, @file_name_1)
+    assert add_status == :ok
+    torrent_pid = Map.get(data, "torrent id")
+
+    assert is_pid(@torrent_impl.whereis(torrent_pid))
+  end
+
+  test "whereis should return undefined when given invalid id from Torrent layer", context do
+    assert @torrent_impl.whereis("fake id") == :undefined
+  end
 end
