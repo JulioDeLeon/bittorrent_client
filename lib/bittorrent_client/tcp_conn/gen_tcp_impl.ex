ed1266e0c7fcb389334a1ae3930701830f3c1c58
Julio de Leon <jangelod@gmail.com>
2018-01-24 06:53:36 -0600

    working on mock

--- lib/bittorrent_client/tcp_conn/gen_tcp_impl.ex
+++ lib/bittorrent_client/tcp_conn/gen_tcp_impl.ex
@@ -1,6 +1,31 @@
-defmodule BittorrentClient.Peer.TCPConn.GenTCPImpl do
+defmodule BittorrentClient.TCPConn.GenTCPImpl do
   @moduledoc """
   :gen_tcp implementation of TCPConn behaviour
   """
-  @behaviour BittorrentClient.Peer.TCPConn
+  @behaviour BittorrentClient.TCPConn
+  alias BittorrentClient.TCPConn, as: TCPConn
+
+  def connect(ip, port, opts \\ [])do
+    {status, socket} = :gen_tcp.connect(ip, port, opts)
+    case status do
+      :ok ->
+        {:ok, %TCPConn{
+            socket: socket,
+            parent_pid: self()
+         }}
+    end
+  end
+
+  def connect(ip, port, opts, timeout) do
+    {status, socket} = :gen_tcp.connect(ip, port, opts, timeout)
+    case status do
+      :ok ->
+        {:ok, %TCPConn{
+            socket: socket,
+            parent_pid: self()
+         }}
+    end
+  end
+
+
 end
