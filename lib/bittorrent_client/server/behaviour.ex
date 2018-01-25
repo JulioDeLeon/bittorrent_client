56ff62d23d6ed33ba48c590e8f1b81b721f1e70e
Julio de Leon <jdeleon@CHIC02RJ9HWG8WN.sea.corp.expecn.com>
2018-01-25 12:57:16 -0600

    revised server behaviour spec and genserver impl to be consistent

--- lib/bittorrent_client/server/behaviour.ex
+++ lib/bittorrent_client/server/behaviour.ex
@@ -2,6 +2,13 @@ defmodule BittorrentClient.Server do
   @moduledoc """
   Behaviour definition for BittorrentClient's server mechanisms
   """
+  alias BittorrentClient.Torrent.Data, as: TorrentData
+  alias Bento.Metainfo.Torrent, as: TorrentMetainfo
+  @type torrent_entry :: %{bitstring() => TorrentMetainfo.t | TorrentData.t}
+  @type torrent_table :: %{bitstring() => %{bitstring() => torrent_entry}}
+  @type http_status :: integer()
+  @type reason :: bitstring()
+  @type torrent_id :: bitstring()
 
   @doc """
   Takes a name of a process to return the PID related to the name.
@@ -11,36 +18,35 @@ defmodule BittorrentClient.Server do
   @doc """
   Returns a GenServer call replay which contains a map of all torrents and their data
   """
-  @callback list_current_torrents(serverName :: String.t()) ::
-              {atom(), {atom(), map()}, tuple()}
+  @callback list_current_torrents(serverName :: String.t()) :: {:ok, torrent_table}
 
   @doc """
-  Attempts to add a new torrent when given a torrent file path. If sucessful, a hash will be returned in GenServer call style. An error message will be returned in GenServer style if unseucessful.
+  Attempts to add a new torrent when given a torrent file path. If sucessful, a hash will be returned.
   """
   @callback add_new_torrent(serverName :: String.t(), torrentFile :: String.t()) ::
-              {atom(), {atom(), String.t()}, tuple()}
+              {:ok, %{bitstring() => torrent_id}} | {:error, {http_status, reason}}
 
   @doc """
-  Attempts to connect a torrent process to it's relative tracker. Returns a success or failure message in GenServer call style.
+  Attempts to connect a torrent process to it's relative tracker.
   """
   @callback connect_torrent_to_tracker(
               serverName :: String.t(),
               torrentID :: String.t()
-            ) :: {atom(), {atom(), String.t()}, tuple()}
+            ) :: {:ok, reason} | {:error, {http_status, reason}}
 
   @doc """
-  Attempts to connect to a torrent process to it's relative tracker asynchronously. does not return feedback in Genserver cast styling.
+  Attempts to connect to a torrent process to it's relative tracker asynchronously.
   """
   @callback connect_torrent_to_tracker_async(
               serverName :: String.t(),
               torrentID :: String.t()
-            ) :: atom()
+            ) :: :ok
 
   @doc """
   Tells torrent process to request peers from the tracker and start requesting data. Will fail if the process has not connect to the tracker yet.
   """
   @callback start_torrent(serverName :: String.t(), torrentID :: String.t()) ::
-              {atom(), {atom(), String.t()}, tuple()}
+              {:ok, reason} | {:error, {http_status, reason}}
 
   @doc """
   Tells torrent process to request peers from the tracker and start request data from peers. This returns In Genserver cast styling.
@@ -48,7 +54,7 @@ defmodule BittorrentClient.Server do
   @callback start_torrent_async(
               serverName :: String.t(),
               torrentID :: String.t()
-            ) :: atom()
+            ) :: :ok
 
   @doc """
   Retrieves torrent information of preccess. Returns in GenServer all style.
@@ -56,7 +62,7 @@ defmodule BittorrentClient.Server do
   @callback get_torrent_info_by_id(
               serverName :: String.t(),
               torrentID :: String.t()
-            ) :: {atom(), {atom(), any()}, tuple()}
+            ) :: {:ok, torrent_entry} | {:error, {http_status, reason}}
 
   @doc """
   Deletes torrent process by id, returns in GenServer style.
@@ -64,7 +70,7 @@ defmodule BittorrentClient.Server do
   @callback delete_torrent_by_id(
               serverName :: String.t(),
               torrentID :: String.t()
-            ) :: {atom(), {atom(), String.t()}, tuple()}
+            ) :: {:ok, %{bitstring => torrent_id | torrent_entry}} | {:error, {http_status, reason}}
 
   @doc """
   Updated the torrent process's data.
@@ -73,20 +79,20 @@ defmodule BittorrentClient.Server do
               serverName :: String.t(),
               torrentID :: String.t(),
               data :: struct()
-            ) :: {atom(), {atom(), String.t()}, tuple()}
+            ) :: {:ok, torrent_table} | {:error, {http_status, reason}}
 
   @doc """
   Updated the torrent process's status by id. The status can be :init, :started, :stopped, finished.
   """
+  @type torrent_status :: :init | :started | :stopped | :finished
   @callback update_torrent_status_by_id(
               serverName :: String.t(),
               torrentID :: String.t(),
-              status :: atom()
-            ) :: {atom(), {atom(), String.t()}, tuple()}
+              status :: torrent_status) :: {:ok, torrent_table} | {:error, {http_status, reason}}
 
   @doc """
-  Deletes all torrents associated with the server. 
+  Deletes all torrents associated with the server.
   """
   @callback delete_all_torrents(serverName :: String.t()) ::
-              {atom(), {atom(), String.t()}, tuple()}
+              {:ok, torrent_table} | {:error, {http_status, reason}}
 end
