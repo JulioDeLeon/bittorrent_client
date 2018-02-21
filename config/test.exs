a7571be94b1c4f36e81266d99605acc94718ce66
Julio de Leon <jangelod@gmail.com>
2018-02-20 19:30:51 -0600

    Fixed config files

--- config/test.exs
+++ config/test.exs
@@ -5,4 +5,4 @@ config :bittorrent_client,
   torrent_impl: BittorrentClient.Torrent.GenServerImpl,
   peer_impl: BittorrentClient.Peer.GenServerImpl,
   tcp_conn_impl: BittorrentClient.TCPConn.InMemoryImpl,
-  http_handle_impl: BittorrentClient.HTTPHandle.InMemoryImpl,
+  http_handle_impl: BittorrentClient.HTTPHandle.InMemoryImpl
