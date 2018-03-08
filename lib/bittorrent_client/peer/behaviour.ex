82d463f04efcc2ac3fa32a5d2c93f41debebcad5
Julio de Leon <jangelod@gmail.com>
2018-03-07 23:45:30 -0600

    updated Bittorrent Peer docs

--- lib/bittorrent_client/peer/behaviour.ex
+++ lib/bittorrent_client/peer/behaviour.ex
@@ -5,6 +5,16 @@ defmodule BittorrentClient.Peer do
 
   @doc """
   Returns the pid of a named peer process
+
+  ## Parameters
+
+    - peer_id :: String which represents a peer process' id which is used to be identified by a torrent process
+
+  ## Examples
+  ```iex
+    iex> Bittorrent.Peer.whereis("AB45EC343324=====")
+    #PID<0.21.0>
+  ```
   """
   @callback whereis(peer_id :: String) :: pid()
 end
