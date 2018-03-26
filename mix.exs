957cc6351a7d3914e69ab0056a307e6aafad8249
Julio de Leon <jangelod@gmail.com>
2018-03-26 12:45:48 -0500

    Bump in version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BittorrentClient.Mixfile do
   def project do
     [
       app: :bittorrent_client,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
