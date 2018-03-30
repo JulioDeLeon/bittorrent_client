4cb8b3f3baa05affcfb9d75ee134c29ca042745e
Julio de Leon <jangelod@gmail.com>
2018-03-29 19:07:28 -0500

    add setup_dev_env function

--- lib/bittorrent_client.ex
+++ lib/bittorrent_client.ex
@@ -10,4 +10,14 @@ defmodule BittorrentClient do
   def stop(_) do
     Application.stop(__MODULE__)
   end
+
+  def setup_dev_env do
+    :observer.start()
+    :debugger.start()
+    :int.ni(Application.get_env(:bittorrent_client, :server_impl))
+    :int.ni(Application.get_env(:bittorrent_client, :torrent_impl))
+    :int.ni(Application.get_env(:bittorrent_client, :peer_impl))
+    :int.ni(Application.get_env(:bittorrent_client, :tcp_conn_impl))
+    :int.ni(Application.get_env(:bittorrent_client, :http_handle_impl))
+  end
 end
