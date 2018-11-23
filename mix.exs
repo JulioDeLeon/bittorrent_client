852401f44e163572073541118f5283eff48f4b2e
Julio de Leon <jangelod@gmail.com>
2018-11-23 14:11:55 -0600

    removed unneeded ecto commands for project

--- mix.exs
+++ mix.exs
@@ -55,8 +55,8 @@ defmodule BittorrentClient.Mixfile do
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+#      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
