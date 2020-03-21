c9c994ded64d5d7212308c2318b823aa495f2b56
Julio de Leon <jangelod@gmail.com>
2020-03-21 17:52:09 -0500

    updated back to poison handler

--- config/dev.exs
+++ config/dev.exs
@@ -68,4 +68,4 @@ config :bittorrent_client,
   torrent_impl: BittorrentClient.Torrent.GenServerImpl,
   peer_impl: BittorrentClient.Peer.GenServerImpl,
   tcp_conn_impl: BittorrentClient.TCPConn.GenTCPImpl,
-  http_handle_impl: BittorrentClient.HTTPHandle.InMemoryImpl
+  http_handle_impl: BittorrentClient.HTTPHandle.HTTPoisonImpl
